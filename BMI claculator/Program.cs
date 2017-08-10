7268fc29ceb63ae4734700b2f7ac7c2ba7cd3495
mehul <mkhosla2@my.centennialcollege.ca>
2017-08-10 00:20:27 -0400

    added splash form

--- BMI claculator/Program.cs
+++ BMI claculator/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*name:mehul khosla
+ student number-300932741
+ description: created the project
+ version:0.1-created project*/
 namespace BMI_claculator
 {
     static class Program
---
+++ BMI claculator/SplashForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*name:mehul khosla
+ student number-300932741
+ date:august 10,2017
+ description:this is the splash form class for BMI calculator
+ version:0.1-created splash form*/
+namespace BMI_claculator
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
